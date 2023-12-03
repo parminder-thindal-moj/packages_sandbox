ea4b6069ebe61a4ecf3af1eeede8f4f1876ec976
Parminder Thindal <parminderthindal@gmail.com>
2023-12-03 20:07:59 +0000

    Test package

---
+++ demo_a/my_test.py
@@ -0,0 +1,5 @@
+from mytest_package.functions import power
+
+x = power(3, 2)
+
+print(f"power:(3, 2) : {x}")
---
+++ demo_a/mypackage/functions.py
@@ -0,0 +1,11 @@
+def sum(x, y):
+    result = x + y
+    return result
+
+def average(x, y):
+    result = (x + y)/2
+    return result
+
+def power(x, y):
+    result = x ** y
+    return result
